bba08cfc1a2254e73d48f6028da853c5b02201db
Karnaukh A.V <karnauhkgb@mail.ru>
2023-05-22 21:50:23 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amountTicket = 13676;
+        int amountMile = 20;
+        int mile = amountTicket / amountMile;
+
+
+        System.out.println("Количество бонусных миль: " + mile);
+
+
+    }
+}
