75ec670374f79281886b4ac04d4a16d87df0fa91
Karnaukh A.V <karnauhkgb@mail.ru>
2023-06-06 20:39:57 +0700

    Reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,10 @@
 public class BonusMilesService {
-   public int calculate(int price) {
-       int mileCost = 20;
-       int result;
-      result = price / 20;
-       return result;
+    public int calculate(int price) {
+        int mileCost = 20;
+        int result;
+        result = price / mileCost;
+        return result;
 
 
-   }
+    }
 }
